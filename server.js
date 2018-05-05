516eb55a1a119e26719098ea595e60d468bd863f
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2018-05-05 11:35:24 +0100

    custome

--- server.js
+++ server.js
@@ -32,40 +32,40 @@ app.get('/', (req, res) => {
     {
       //Head.hbs variables
       keywords: '',
-      metaDescription: 'Welcome to the Beute Clinic, podiatry service and foot care specialists based in Marlborough. Book and appointment with one of our excellent podiatrists or visit the website to check the treatments available',
+      metaDescription: '',
       pageTitle: 'Estudia Fácil BCN',
 
       //Header.hbs variables
       logo: '',
-      logoCaption: 'Beute Clinic logo',
-      logoTitle: 'Welcome to the Beute Clinic',
+      logoCaption: 'Estudia Fácil BCN logo',
+      logoTitle: 'Bienvenidos a Estudia Fácil BCN',
       // menu1: '',
       // menu2: '',
       // menu3: '',
       // menu4: '',
       // menu5: '',
       //Hero variables
-      h1Title: 'Bienvenidos a Estudia Fácil bcn',
+      h1Title: 'Bienvenidos a Estudia Fácil BCN',
 
-      webmail: 'hsvbeute@gmail.com',
+      webmail: 'estudiafacilbcn@gmail.com',
       twitter_url: '',
-      facebook_url: 'https://www.facebook.com/thebeuteclinic/',
+      facebook_url: '',
       googleplus_url: '',
-      instagram_url: 'https://www.instagram.com/explore/locations/1690143194594857/hannah-beute-clinic/',
-      linkedin_url: 'https://www.linkedin.com/in/hannah-beute-356044151/',
+      instagram_url: '',
+      linkedin_url: '',
 
       //Maps
-      address: 'Unit K, The Wagon Yard, London Road, Marlborough, SN8 1LH',
-      map_link: 'https://www.google.co.uk/maps/place/The+Beute+Clinic/@51.420501,-1.726128,15z/data=!4m2!3m1!1s0x0:0x7157a3cfd58b1f7b?sa=X&ved=0ahUKEwjmsJbLpOfaAhUKDMAKHRq0AMQQ_BIIfjAK',
+      address: 'C/Muntaner, 120',
+      map_link: '',
 
       //Services variables
-      cancellationPolicy1: 'The Beute Clinic operates a cancellation policy. Our policy is similar to many other medical clinics and we ask all patients kindly to adhere to it.',
-      cancellationPolicy2: 'Should you wish to cancel or reschedule an appointment we simply ask you to give a minimum of 24 hours notice for our shorter 30 minute appointments and 48 hours notice for our longer appointments such as our biomechanical assessments, nail surgery appointments and home visits. If this minimum is not adhered to, we reserve the right to charge the full treatment cost of the appointment.',
-      cancellationPolicy3: 'We are aware that from time to time individual circumstances dictate that an appointment will be missed or less than the 24 hours notice will be given. On such occasions we can be lenient but frequent missed appointments can be very disruptive to the smooth running of the clinic and can also be inconvenient to other patients that require an appointment slot',
+      cancellationPolicy1: '',
+      cancellationPolicy2: '',
+      cancellationPolicy3: '',
 
 
       //Contact Variables
-      businessPhone: '01672288943, 07707697396'
+      businessPhone: '(+34) 123 123 123'
 
 
 
@@ -76,14 +76,17 @@ app.get('/', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const output = `<p>You have a new contact request</p>
-  <h3>Contact Details</h3>
+  const output = `<p>Tienes una nueva solicitud de contacto</p>
+  <h3>Detalles del contacto</h3>
   <ul>
-    <li>Name: ${req.body.name}</li>
+    <li>Nombre: ${req.body.name}</li>
     <li>Email: ${req.body.email}</li>
-    <li>Phone: ${req.body.phone}</li>
-    <li>Subject: ${req.body.subject}</li>
-    <li>Message: ${req.body.message}</li>
+    <li>Telf: ${req.body.phone}</li>
+    <li>Materias de interés: ${req.body.materias}</li>
+    <li>Nivel educativo: ${req.body.nivel}</li>
+    <li>Telf: ${req.body.phone}</li>
+    <li>Asunto: ${req.body.subject}</li>
+    <li>Mensaje: ${req.body.message}</li>
     </ul>`;
 
   // create reusable transporter object using the default SMTP transport
@@ -105,7 +108,7 @@ app.post('/send', (req, res) => {
   let mailOptions = {
     from: process.env.MAIL_FROM, // sender address
     to: process.env.MAIL_TO, // list of receivers
-    subject: 'Contact request', // Subject line
+    subject: 'Solicitud de contacto', // Subject line
     text: 'Hello world', // plain text body
     html: output // html body
   };
@@ -122,7 +125,7 @@ app.post('/send', (req, res) => {
 
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-    res.render('partials/thanks', { businessName: 'The Beute Clinic' });
+    res.render('partials/thanks', { businessName: 'Estudia Fácil BCN' });
 
 
 
